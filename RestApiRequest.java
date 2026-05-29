56d15801b3212e034948a8978c09cf13315ca7e0
Vitor Queiroz <vitor.qn2004@gmail.com>
2026-05-29 19:25:57 -0300

    Feature:TranscriptAudio

--- RestApiRequest.java
+++ RestApiRequest.java
@@ -1,15 +1,35 @@
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.file.Path;
 
 public class RestApiRequest {
     public static void main(String[] args) throws Exception {
 
-        HttpRequest postResquest = HttpRequest.newBuilder()
-        .uri(new URI("https://api.assemblyai.com/v2/transcript"))
-        .header("Authorization", Constantes.API_KEY)
-        .POST(HttpRequest.BodyPublishers.ofString("{ \"audio_url\": \"" + UploadAudio.uploadAudio(Path.of("audios/AudioApiTest.ogg")) + "\" }"))
-        .build();
+        TranscriptAudio transcriptAudio = new TranscriptAudio();
+        transcriptAudio.setAudio_url(UploadAudio.uploadAudio(Path.of("audios/AudioApiTest.ogg")));
+        String jsonRequest = """
+                {
+                  "audio_url": "%s",
+                  "speech_models": ["universal-3-pro"]
+                }
+                """.formatted(transcriptAudio.getAudio_url());
 
+        System.out.println("JSON Body: " + jsonRequest);
+
+        HttpRequest postResquest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                .header("Authorization", Constantes.API_KEY)
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse<String> postResponse = client.send(postResquest, BodyHandlers.ofString());
+
+        System.out.println("POST Response: " + postResponse.body());
     }
+
 }
---
+++ TranscriptAudio.java
@@ -0,0 +1,12 @@
+public class TranscriptAudio {
+    
+    private String audio_url;
+
+    public String getAudio_url() {
+        return audio_url;
+    }
+
+    public void setAudio_url(String audio_url) {
+        this.audio_url = audio_url;
+    }
+}
